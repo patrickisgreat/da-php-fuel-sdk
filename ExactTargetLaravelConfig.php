1bc6d259845c73c6113dfbf71e8fae66b6ec7ff9
Patrick Bennett <patrickisgreat@gmail.com>
2018-04-23 19:04:38 -0400

    Create ExactTargetLaravelConfig.php

---
+++ ExactTargetLaravelConfig.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'appsignature' => 'none',
+    'clientid' => 'wafdnppxccaccme56a3v77zm',
+    'clientsecret' => 'HftPagJF8UUZBG9XYwrHuHuS',
+    'defaultwsdl' => 'https://webservice.exacttarget.com/etframework.wsdl'
+);
